497b79200735e1d789e2f34c9042bfb523791c9d
lucie_rc <104382956+LucieRC@users.noreply.github.com>
2023-04-12 18:49:31 +0300

    For Marie

--- Assignment 3/MARIMA_part2_3.r
+++ Assignment 3/MARIMA_part2_3.r
@@ -31,53 +31,53 @@ ln_diffed_Everything$InterestRate <- Everything$InterestRate[1:121]
 ln_diffed_Everything$InflationRate <- Everything$InflationRate[1:121]
 
 
-ar = c(1)
-ma = c(0)
-model_structure <- define.model(kvar=7, ar=ar, ma=ma, rem.var=c(1), reg.var=c(6,7), indep=NULL) #
-marima_model <- marima(t(Everything), means=1,
-                  ar.pattern=model_structure$ar.pattern, ma.pattern=model_structure$ma.pattern,
-                  Check=FALSE, Plot='log.det', penalty=0.0)
-sl <- step.slow(marima_model, Everything)
-arma_forecast <-  arma.forecast(t(Everything[1:122,]), nstart=0, nstep=122, marima=sl)
-plot(seq(1,122,1), arma_forecast$forecasts[2,])
+# ar = c(1)
+# ma = c(0)
+# model_structure <- define.model(kvar=7, ar=ar, ma=ma, rem.var=c(1), reg.var=c(6,7), indep=NULL) #
+# marima_model <- marima(t(Everything), means=1,
+#                   ar.pattern=model_structure$ar.pattern, ma.pattern=model_structure$ma.pattern,
+#                   Check=FALSE, Plot='log.det', penalty=0.0)
+# sl <- step.slow(marima_model, Everything)
+# arma_forecast <-  arma.forecast(t(Everything[1:122,]), nstart=0, nstep=122, marima=sl)
+# plot(seq(1,122,1), arma_forecast$forecasts[2,])
 
 
-ar = c(1)
-ma = c(1)
-model_structure <- define.model(kvar=7, ar=ar, ma=ma, rem.var=c(1), reg.var=c(6,7), indep=NULL) #
-marima_model <- marima(t(Everything), means=1,
-                  ar.pattern=model_structure$ar.pattern, ma.pattern=model_structure$ma.pattern,
-                  Check=FALSE, Plot='log.det', penalty=0.0)
-sl <- step.slow(marima_model, Everything)
-arma_forecast <-  arma.forecast(t(Everything[1:122,]), nstart=0, nstep=122, marima=sl)
-plot(seq(1,122,1), arma_forecast$forecasts[2,])
-
-
-
-ar = c(1)
-ma = c(0)
-model_structure <- define.model(kvar=7, ar=ar, ma=ma, rem.var=c(1), reg.var=c(6,7), indep=NULL) #
-marima_model <- marima(t(diffed_Everything), means=1,
-                  ar.pattern=model_structure$ar.pattern, ma.pattern=model_structure$ma.pattern,
-                  Check=FALSE, Plot='log.det', penalty=0.0)
-sl <- step.slow(marima_model, diffed_Everything)
-arma_forecast_no_ma_all_diffed <- arma.forecast(t(Everything[1:122,]), nstart=0, nstep=122, marima=sl)
-plot(seq(1,121,1), arma_forecast_no_ma_all_diffed$forecasts[2,1:121])
-plot(seq(1,121,1), diffed_Everything[,2])
-#check with the real distrib
+# ar = c(1)
+# ma = c(1)
+# model_structure <- define.model(kvar=7, ar=ar, ma=ma, rem.var=c(1), reg.var=c(6,7), indep=NULL) #
+# marima_model <- marima(t(Everything), means=1,
+#                   ar.pattern=model_structure$ar.pattern, ma.pattern=model_structure$ma.pattern,
+#                   Check=FALSE, Plot='log.det', penalty=0.0)
+# sl <- step.slow(marima_model, Everything)
+# arma_forecast <-  arma.forecast(t(Everything[1:122,]), nstart=0, nstep=122, marima=sl)
+# plot(seq(1,122,1), arma_forecast$forecasts[2,])
 
 
 
-ar = c(1)
-ma = c(0)
-model_structure <- define.model(kvar=7, ar=ar, ma=ma, rem.var=c(1), reg.var=c(6,7), indep=NULL) #
-marima_model <- marima(t(diffed_Everything), means=1,
-                  ar.pattern=model_structure$ar.pattern, ma.pattern=model_structure$ma.pattern,
-                  Check=FALSE, Plot='log.det', penalty=0.0)
-sl <- step.slow(marima_model, diffed_Everything)
-arma_forecast_no_ma_all_diffed <- arma.forecast(t(Everything[1:122,]), nstart=0, nstep=122, marima=sl)
-plot(seq(1,121,1), arma_forecast_no_ma_all_diffed$forecasts[2,1:121])
-plot(seq(1,121,1), diffed_Everything[,2])
+# ar = c(1)
+# ma = c(0)
+# model_structure <- define.model(kvar=7, ar=ar, ma=ma, rem.var=c(1), reg.var=c(6,7), indep=NULL) #
+# marima_model <- marima(t(diffed_Everything), means=1,
+#                   ar.pattern=model_structure$ar.pattern, ma.pattern=model_structure$ma.pattern,
+#                   Check=FALSE, Plot='log.det', penalty=0.0)
+# sl <- step.slow(marima_model, diffed_Everything)
+# arma_forecast_no_ma_all_diffed <- arma.forecast(t(Everything[1:122,]), nstart=0, nstep=122, marima=sl)
+# plot(seq(1,121,1), arma_forecast_no_ma_all_diffed$forecasts[2,1:121])
+# plot(seq(1,121,1), diffed_Everything[,2])
+# #check with the real distrib
+
+
+
+# ar = c(1)
+# ma = c(0)
+# model_structure <- define.model(kvar=7, ar=ar, ma=ma, rem.var=c(1), reg.var=c(6,7), indep=NULL) #
+# marima_model <- marima(t(diffed_Everything), means=1,
+#                   ar.pattern=model_structure$ar.pattern, ma.pattern=model_structure$ma.pattern,
+#                   Check=FALSE, Plot='log.det', penalty=0.0)
+# sl <- step.slow(marima_model, diffed_Everything)
+# arma_forecast_no_ma_all_diffed <- arma.forecast(t(Everything[1:122,]), nstart=0, nstep=122, marima=sl)
+# plot(seq(1,121,1), arma_forecast_no_ma_all_diffed$forecasts[2,1:121])
+# plot(seq(1,121,1), diffed_Everything[,2])
 
 
 
@@ -90,5 +90,35 @@ marima_model <- marima(t(ln_diffed_Everything), means=1,
                   Check=FALSE, Plot='log.det', penalty=0.0)
 sl <- step.slow(marima_model, ln_diffed_Everything)
 arma_forecast <- arma.forecast(t(ln_diffed_Everything[1:122,]), nstart=0, nstep=122, marima=sl)
-plot(seq(1,121,1), arma_forecast$forecasts[2,1:121])
-plot(seq(1,121,1), ln_diffed_Everything[,2])
+# plot(seq(1,121,1), arma_forecast$forecasts[2,1:121])
+# plot(seq(1,121,1), ln_diffed_Everything[,2])
+
+# forecast_2 <- exp(diffinv(arma_forecast$forecasts[2,1:121], xi=ln_Everything[1,2]))
+
+forecast_original <- list()
+for (i in seq(2,5,1)) {
+    len <- length(forecast_original)
+    forecast_original[[len+1]] <- exp(diffinv(arma_forecast$forecasts[i,1:121], xi=ln_Everything[1,i]))
+}
+
+
+# plot(seq(1,122,1), forecast_2)
+# plot(seq(1,122,1), Everything[,2])
+
+# typeof(arma_forecast$forecasts[2,1:121])
+
+# (data.frame(ln_diffed_Everything))
+# c()
+# diffinv(ln_diffed_Everything[,2:5],xi=ln_Everything[1,2:5])
+# ln_Everything[,2]
+# ln_Everything
+
+
+
+
+# line(seq(1,122,1), Everything[,2])
+ggplot(seq(1,122,1), unlist(forecast_original[1]))
+plot(seq(1,122,1), unlist(forecast_original[2]))
+line(seq(1,122,1), unlist(forecast_original[3]))
+plot(seq(1,122,1), Everything[,5])
+
